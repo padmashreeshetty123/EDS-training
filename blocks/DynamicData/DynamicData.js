650d7965a06ae67126f98f5c57b68888ae7c9eb2
padmashree <padmashreeshetty245@gmail.com>
2024-06-27 19:17:06 +0530

    forms & dynamic data assignment

--- blocks/DynamicData/DynamicData.js
+++ blocks/DynamicData/DynamicData.js
@@ -1,12 +1,12 @@
-let start_size=0;
-let end_size=20;
+let startSize=0;
+let endSize=20;
 export default async function decorate(block)
 {
     const response=await fetch("http://localhost:3000/redirects.json");
     const dynamicBlock=document.querySelector(".dynamicdata");
     const data=await response.json();
-    let ul=document.createElement('ul');
-    data.data.slice(start_size,end_size).forEach(element => {
+    let ul = document.createElement('ul');
+    data.data.slice(startSize,endSize).forEach(element => {
         const li=document.createElement('li');
         li.textContent=element.SOURCE+" : "+element.DESTINATION;
         ul.append(li);
@@ -20,20 +20,20 @@ export default async function decorate(block)
       
         event.preventDefault();
        
-        if(start_size>0){
+        if(startSize>0){
             dynamicBlock.removeChild(ul);
-            console.log(start_size-20,start_size);
-           ul=document.createElement('ul');
-            data.data.slice(start_size-20,start_size).forEach(element => {
+            console.log(startSize-20,startSize);
+           ul = document.createElement('ul');
+            data.data.slice(startSize-20,startSize).forEach(element => {
                 console.log("dwf");
                 const li=document.createElement('li');
                 li.textContent=element.SOURCE+" : "+element.DESTINATION;
                 ul.append(li);
-                console.log(end_size);
+                console.log(endSize);
             });
             dynamicBlock.append(ul); 
-    start_size=start_size-20;
-    end_size=start_size;
+    startSize = startSize-20;
+    endSize = startSize;
             }});
  
 
@@ -43,20 +43,20 @@ export default async function decorate(block)
     nextButton.addEventListener('click',(event)=>{
         event.preventDefault();
      
-      if(end_size<data.total){
+      if(endSize<data.total){
         dynamicBlock.removeChild(ul);
-        console.log(end_size,(data.total-(end_size))/20>=1?end_size+20:end_size+(data.total-(end_size)));
-       ul=document.createElement('ul');
-        data.data.slice(end_size,(data.total-(end_size))/20>=1?end_size+20:end_size+(data.total-(end_size))).forEach(element => {
+        console.log(endSize,(data.total-(endSize))/20>=1?endSize+20:endSize+(data.total-(endSize)));
+       ul = document.createElement('ul');
+        data.data.slice(endSize,(data.total-(endSize))/20>=1?endSize+20:endSize+(data.total-(endSize))).forEach(element => {
             console.log("dwf");
-            const li=document.createElement('li');
-            li.textContent=element.SOURCE+" : "+element.DESTINATION;
+            const li = document.createElement('li');
+            li.textContent = element.SOURCE+" : "+element.DESTINATION;
             ul.append(li);
-            console.log(end_size);
+            console.log(endSize);
         });
         dynamicBlock.append(ul); 
-start_size=end_size;
-end_size=(data.total-(end_size))/20>=1?end_size+20:end_size+(data.total-(end_size));
+startSize = endSize;
+endSize = (data.total-(endSize))/20>=1?endSize+20:endSize+(data.total-(endSize));
         }
 });
     prevButton.href=nextButton.href="/";
