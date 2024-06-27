026b32b378e21a02aec963b3e9ec2ce495f49dc9
padmashree <padmashreeshetty245@gmail.com>
2024-06-27 19:20:25 +0530

    forms & dynamic data assignment

--- blocks/DynamicData/DynamicData.js
+++ blocks/DynamicData/DynamicData.js
@@ -1,31 +1,31 @@
 let startSize = 0;
 let endSize = 20;
 export default async function decorate() {
-  const response = await fetch("http://localhost:3000/redirects.json");
-  const dynamicBlock = document.querySelector(".dynamicdata");
+  const response = await fetch('http://localhost:3000/redirects.json');
+  const dynamicBlock = document.querySelector('.dynamicdata');
   const data = await response.json();
-  let ul = document.createElement("ul");
+  let ul = document.createElement('ul');
   data.data.slice(startSize, endSize).forEach((element) => {
-    const li = document.createElement("li");
-    li.textContent = element.SOURCE + " : " + element.DESTINATION;
+    const li = document.createElement('li');
+    li.textContent = element.SOURCE + ' : ' + element.DESTINATION;
     ul.append(li);
   });
   dynamicBlock.append(ul);
   console.log(data.total);
-  const prevButton = document.createElement("a");
-  prevButton.textContent = "Prev";
-  prevButton.classList.add("prev", "button");
-  prevButton.addEventListener("click", (event) => {
+  const prevButton = document.createElement('a');
+  prevButton.textContent = 'Prev';
+  prevButton.classList.add('prev', 'button');
+  prevButton.addEventListener('click', (event) => {
     event.preventDefault();
 
     if (startSize > 0) {
       dynamicBlock.removeChild(ul);
       console.log(startSize - 20, startSize);
-      ul = document.createElement("ul");
+      ul = document.createElement('ul');
       data.data.slice(startSize - 20, startSize).forEach((element) => {
-        console.log("dwf");
-        const li = document.createElement("li");
-        li.textContent = element.SOURCE + " : " + element.DESTINATION;
+        console.log('dwf');
+        const li = document.createElement('li');
+        li.textContent = element.SOURCE + ' : ' + element.DESTINATION;
         ul.append(li);
         console.log(endSize);
       });
@@ -35,10 +35,10 @@ export default async function decorate() {
     }
   });
 
-  const nextButton = document.createElement("a");
-  nextButton.textContent = "Next";
-  nextButton.classList.add("next", "button");
-  nextButton.addEventListener("click", (event) => {
+  const nextButton = document.createElement('a');
+  nextButton.textContent = 'Next';
+  nextButton.classList.add('next', 'button');
+  nextButton.addEventListener('click', (event) => {
     event.preventDefault();
 
     if (endSize < data.total) {
@@ -49,7 +49,7 @@ export default async function decorate() {
           ? endSize + 20
           : endSize + (data.total - endSize)
       );
-      ul = document.createElement("ul");
+      ul = document.createElement('ul');
       data.data
         .slice(
           endSize,
@@ -58,9 +58,9 @@ export default async function decorate() {
             : endSize + (data.total - endSize)
         )
         .forEach((element) => {
-          console.log("dwf");
-          const li = document.createElement("li");
-          li.textContent = element.SOURCE + " : " + element.DESTINATION;
+          console.log('dwf');
+          const li = document.createElement('li');
+          li.textContent = element.SOURCE + ' : ' + element.DESTINATION;
           ul.append(li);
           console.log(endSize);
         });
@@ -72,7 +72,7 @@ export default async function decorate() {
           : endSize + (data.total - endSize);
     }
   });
-  prevButton.href = nextButton.href = "/";
+  prevButton.href = nextButton.href = '/';
   dynamicBlock.append(prevButton);
   dynamicBlock.append(nextButton);
 }
